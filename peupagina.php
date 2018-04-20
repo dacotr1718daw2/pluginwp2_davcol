eedf5d17bd961b7f3628464ac3420090cd295fc2
root <root@debian9v1.fjeclot.net>
2018-04-20 20:27:00 +0200

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/dacotr1718daw2/pluginwp2_davcol.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: David Colomo
+Author URI: https://github.com/dacotr1718daw2
+License: GNU GPLv3
+*/
+?>
